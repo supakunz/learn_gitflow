9709eeda791bbc068ccc35f420f4127facf4899e
Supakun <supakunt.thata@gmail.com>
2025-06-19 17:46:28 +0700

    feat : test comment3

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
         <p>GG EZ2.</p>
         <p>test1</p>
         <p>test2</p>
+        <p>test3</p>
 
         <div style={{ display: "flex", gap: "10px", justifyContent: "center" }}>
           <button onClick={() => setCount((count) => count + 1)}>
